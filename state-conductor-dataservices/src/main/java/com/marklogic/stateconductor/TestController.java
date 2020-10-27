157a154b469552475d16c73d206e074f9d3a3a0f
Anthony Clavio <aclavio@gmail.com>
2020-10-27 11:26:29 -0400

    moved public folder to be included in jar test utility for auto monitoring status of a state machine

--- state-conductor-dataservices/src/main/java/com/marklogic/stateconductor/TestController.java
+++ state-conductor-dataservices/src/main/java/com/marklogic/stateconductor/TestController.java
@@ -1,11 +1,14 @@
 package com.marklogic.stateconductor;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Lists;
 import com.marklogic.StateConductorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -23,14 +26,21 @@ public class TestController {
   }
 
   @GetMapping("/api/state-machine")
-  public ObjectNode listAllStateMachines() {
+  public List<String> listAllStateMachines() {
     ObjectNode result = stateConductorService.getStateMachine(null);
-    return result;
+    List<String> names = Lists.newArrayList(result.fieldNames());
+    return names;
   }
 
   @GetMapping("/api/state-machine/{name}")
   public ObjectNode listStateMachines(@PathVariable String name) {
     ObjectNode result = stateConductorService.getStateMachine(name);
+    return result;
+  }
+
+  @GetMapping("/api/state-machine/{name}/status")
+  public ObjectNode listStateMachinesStatus(@PathVariable String name) {
+    ObjectNode result = stateConductorService.getStateMachineStatus(Arrays.stream(new String[]{ name }), null, null, false);
     return result;
   }
 }
